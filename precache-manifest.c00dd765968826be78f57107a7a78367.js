66b753b9960d55932e6de1c32bc2c230727653ef
mchyun <mchyun@connect.hku.hk>
2022-06-01 11:34:52 +0800

    Updates

---
+++ precache-manifest.c00dd765968826be78f57107a7a78367.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a40f9389042eb9bf3695",
+    "url": "/static/css/main.a24d6103.chunk.css"
+  },
+  {
+    "revision": "a40f9389042eb9bf3695",
+    "url": "/static/js/main.f427ea61.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "472f472de4ff1afb7a7c",
+    "url": "/static/js/2.65eeef47.chunk.js"
+  },
+  {
+    "revision": "fd8e610a42836c354572457303c2c1b1",
+    "url": "/index.html"
+  }
+];
